ed8d8f84c6a04f8a978c5de5f0178b0ac1471d59
Joseph Cunningham <joechamm@outlook.com>
2020-10-24 00:07:33 -0400

    add Datasource class

--- app/src/main/java/com/guineacoder/affirmations/MainActivity.kt
+++ app/src/main/java/com/guineacoder/affirmations/MainActivity.kt
@@ -2,6 +2,9 @@ package com.guineacoder.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.guineacoder.affirmations.data.Datasource
+import javax.sql.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/guineacoder/affirmations/model/Affirmation.kt
@@ -0,0 +1,5 @@
+package com.guineacoder.affirmations.model
+
+data class Affirmation(val stringResourceId: Int) {
+
+}
