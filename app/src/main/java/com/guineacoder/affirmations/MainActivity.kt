b6e79af1e76f577c2e89eec54681acd90a7438ed
Joseph Cunningham <joechamm@outlook.com>
2020-10-24 01:48:36 -0400

    add images

--- app/src/main/java/com/guineacoder/affirmations/MainActivity.kt
+++ app/src/main/java/com/guineacoder/affirmations/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.guineacoder.affirmations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.guineacoder.affirmations.adapter.ItemAdapter
 import com.guineacoder.affirmations.data.Datasource
-import javax.sql.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performace if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }
